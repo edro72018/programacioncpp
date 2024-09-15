d185f75277e60bd499b454688033ee46d618e0e9
edro72018 <edro7201@gmail.com>
2024-09-15 05:51:39 -0600

    Ejercicios de principiante

---
+++ ejercicio3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+int opcion;
+bool cont = true;
+do{
+cout << "===Menú de operaciones ===" << endl;
+cout << "1. Sumar dos numeros" << endl;
+cout << "2. Restar dos numeros" << endl;
+cout << "3. Multiplicar dos numeros" << endl;
+cout << "4. Dividir dos numeros" << endl;
+cout << "5. Calcular el promedio de varios numeros" << endl;
+cout << "6. Salir" << endl;
+cout << "Seleccione una opcion (1-6): ";
+cin >> opcion;
+switch(opcion){
+case 1:
+int num1, num2, sum;
+cout << "ingrese un numero entero" << endl;
+cin >> num1;
+cout << "ingrese otro numero entero" << endl;
+cin >> num2;
+sum = num1 + num2 ;
+cout << "La suma es: " << sum << endl;
+break;
+}
+}while(cont);
+return 0;
+}
+ 
---
+++ example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num1, num2, sum;
+cin >> num1;
+cout << num1 << endl;
+cin >> num2;
+sum = num1 + num2;
+cout << num1 << " + " << num2 <<  " = " << sum;
+return 0;
+}
